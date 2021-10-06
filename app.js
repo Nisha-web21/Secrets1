eba32b263e919d32a346e6aa45e8db749bc43040
Nisha <you@example.com>
2021-10-05 17:37:58 -0700

    Third use of commit after corrections and adding env variables

--- app.js
+++ app.js
@@ -46,7 +46,13 @@ app.get("/register",function(req,res){
   res.render("register");
 })
 
-app.listen(3000,function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port,function(){
   console.log("server started at port 3000");
   //res.render("test");
 
